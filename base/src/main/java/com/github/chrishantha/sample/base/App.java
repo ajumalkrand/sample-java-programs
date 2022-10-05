00b29792295bcc63658e19b20413de52135150f4
ajumalkrand <113851472+ajumalkrand@users.noreply.github.com>
2022-10-05 13:16:57 +0530

    Update App.java

--- base/src/main/java/com/github/chrishantha/sample/base/App.java
+++ base/src/main/java/com/github/chrishantha/sample/base/App.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.chrishantha.sample.basxxxe;
+package com.github.chrishantha.sample.base;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
